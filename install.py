c6d0628b51c1eb81d7aad29ecc0e164a4bb33ba8
pauljako <paulifey18@gmail.com>
2024-01-05 18:51:55 +0100

    Bug Fix

--- install.py
+++ install.py
@@ -1,7 +1,16 @@
 #!/bin/python3
 from pathlib import Path
+import os
 
 Path('boundaries.py').symlink_to('../../bin/main.py')
+
+os.system("chmod +x main.py")
+
+if not os.path.exists("../../var/bnd-repo/repos"):
+    Path("../../var/bnd-repo/repos").mkdir()
+
+    with open("../../var/bnd-repo/repos/index.json", "w") as f:
+        f.write("{}")
 
 import main
 
